e226308bcfc5b2606deb845283c67544c98ac5f0
Richard Craven <rcraven85@gmail.com>
2017-01-06 09:52:41 -0800

    fixed button nav

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(function() {
 	$.scrollify({
 			section : ".section",
-			sectionName : "",
+			sectionName : "section-name",
 			interstitialSection : "",
 			easing: "easeOutExpo",
 			scrollSpeed: 1100,
@@ -18,44 +18,73 @@ $(function() {
 			afterRender:function() {}
 	});
 });
+$('.homeButton').click(function(){
+	$.scrollify.move("#home");
+})
+$('.portfolioButton').click(function(){
+	$.scrollify.move("#portfolio");
+})
+$('.aboutButton').click(function(){
+	$.scrollify.move("#about");
+})
+
+
+
+window.onload = function(){
+$('li.homeButton').addClass('active')
+}
+
+
+$('body').addClass('stop-scrolling')
+
+$('body').css('background-color','black')
+
+// $('body').css('background-color','silver')
+// var image = document.images[0];
+// var downloadingImage = new Image();
+// downloadingImage.src = "images/beautiful.jpg";
+// downloadingImage.onload = function(){
+//     image.src = this.src;   
+//     console.log(image.src)
+// };
+// $('body').css('background-image','url(images/beautiful.jpg)')
+
+
+$('body').css('background-image','url(images/beautiful_smaller.jpg)')
+
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+	$('.navbar').css('display','block');
+	$('.email').css('display','block');
+	$('.phonenum').css('display','block')
+	$('body').removeClass('stop-scrolling')
+})
+
+
+
+////
+
 
 var app = angular.module('myPage',['ngAnimate']);
-// ,'ui.bootstrap'
-app.directive('backImg', function(){
-    return function(scope, element, attrs){
-        attrs.$observe('backImg', function(value) {
-            element.css({
-                'background-image': 'url(' + value +')',
-                'background-size' : '100% 100%',
-                'background-repeat': 'no-repeat'
 
-            });
-        });
-    };
-});
+// app.directive('backImg', function(){
+//     return function(scope, element, attrs){
+//         attrs.$observe('backImg', function(value) {
+//             element.css({
+//                 'background-image': 'url(' + value +')',
+//                 'background-size' : '100% 100%',
+//                 'background-repeat': 'no-repeat'
+
+//             });
+//         });
+//     };
+// });
 
 
 app.controller('MainController', function($scope, $interval){
 	window.scope=$scope
-	$scope.showMain = true;
-	$scope.showPortfolio = false;
-	$scope.showResume = false;
 	
-	$scope.homeClick = function(){
-		$scope.showMain = true;
-		$scope.showPortfolio = false;
-		$scope.showResume = false;
-	}
-	$scope.portfolioClick = function(){
-		$scope.showMain = false;
-		$scope.showPortfolio = true;
-		$scope.showResume = false;
-	}
-	$scope.resumeClick = function(){
-		$scope.showMain = false;
-		$scope.showPortfolio = false;
-		$scope.showResume = true;
-	}
+	
 	$scope.cahootsLink = function(){
 		window.open("http://cahoots.live")
 	}
@@ -87,69 +116,51 @@ function potatoeScroll(ev){
 }
 window.onscroll=potatoeScroll
 
-
-$('.showHead').click(function(){
-	function showHead() {
-	$('.head')[0].style.display = 'block'
-	}
-	var go = setTimeout(showHead, 5000);
-})
-
-$('.papaya').click(function () {
-      $('#wrapper').toggleClass('toggled');
-}); 
-
-$('#richard').click(function () {
-      $('#wrapper').toggleClass('toggled');
+var aboutPage = $("#about").position().top; 
+var portfolioPage = $("#portfolio").position().top; 
+var homePage = $("#home").position().top; 
+$(window).on("scroll", function() {
+  var scrollPosition = scrollY || pageYOffset;
+  if (scrollPosition === aboutPage){
+  	$('.head')[0].style.display = 'block'
+  	var go = setTimeout(showHead, 3500)
+  	$('li.aboutButton').addClass('active')
+  	$('li.homeButton').removeClass('active')
+  	$('li.portfolioButton').removeClass('active')
+  }
+  if (scrollPosition === portfolioPage ){
+  	$('li.aboutButton').removeClass('active')
+  	$('li.homeButton').removeClass('active')
+  	$('li.portfolioButton').addClass('active')
+  }
+  if (scrollPosition === homePage){
+  	$('li.homeButton').addClass('active')
+  	$('li.aboutButton').removeClass('active')
+  	$('li.portfolioButton').removeClass('active')
+  }			
 });
-// $('.mouseOverDiv').mouseenter(function(){
-// 	if (!$('#wrapper').hasClass('toggled')){
-// 	    $('#wrapper').addClass("toggled")
-// 	}
-// 	$('.mouseOverDiv').unbind('mouseenter')
-// })
-// $('.mouseOverDiv').mouseleave(function(){
-// 	// if ($('#wrapper').hasClass('toggled')){		
-// 	$('#wrapper').toggleClass('toggled')
-// 	// }
-// })
+
+function showHead() {
+$('.head')[0].style.display = 'block'
+var go = setTimeout(showHead, 5000);
+}
+
+
+
+
+
+
+// $('.papaya').click(function () {
+//       $('#wrapper').toggleClass('toggled');
+// }); 
+
+// $('#richard').click(function () {
+//       $('#wrapper').toggleClass('toggled');
+// });
 $('#sidebar-wrapper').hover(function () {
      $(this).stop().animate({left:"0px", opacity:1},500);     
    },function () {
        $(this).stop().animate({left: '-150px', opacity:0  },500);     
 });
-// $('#richard').click(function () {
-// 	if($('#wrapper').style.opacity = 0){
-// 	$('#wrapper').stop().animate({left:"0px", opacity:1},500);
-// 	}
-//     else {     
-//     $('#wrapper').stop().animate({left: '-150px', opacity:0  },500);  
-//     }   
-// });
 
 
-
-
-
-
-
-// $('.btn').click(function() {
-//     $('.modal')
-//         .prop('class', 'modal fade') // revert to default
-//         .addClass( $(this).data('direction') );
-//     $('.modal').modal('show');
-// });
-
-// $(function(){
-// $('.demo div').inview();
-// });
-
-// $(function(){
-// $('.demo div').inview({
-// 'viewFactor': 0.3,
-// 'onEnter': function($object) {
-// },
-// 'onLeave': function($object) {
-// }
-// });
-// });
